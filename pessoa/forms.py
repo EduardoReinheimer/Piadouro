8816210c378c9c61111e1616f1d2116139f707a1
darlan <dcpoffo@gmail.com>
2020-08-25 22:23:30 -0300

    Melhorias na tela de cadastro de usuário

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -1,11 +1,25 @@
-from django.forms import ModelForm, inlineformset_factory
+from django.forms import ModelForm, inlineformset_factory, CharField, PasswordInput, Select
 from django.contrib.auth.models import User 
 from pessoa.models import Perfil
 
 class UserForm(ModelForm):
-
+    password2 = CharField(max_length=60, required=True, widget=PasswordInput())
     class Meta:
         model = User
-        exclude = ['is_staff', 'is_active', 'date_joined', 'last_login']
+        exclude = [
+            'is_staff', 
+            'is_active', 
+            'date_joined', 
+            'last_login', 
+            'groups', 
+            'user_permissions', 
+            'is_superuser',
+        ]
+        widgets = {
+            'password': PasswordInput()
+        }
 
-UserProfileForm = inlineformset_factory(User, Perfil, exclude=('usuario',))
+class UserProfileForm(ModelForm):
+    class Meta: 
+        model = Perfil
+        exclude = ['usuario']

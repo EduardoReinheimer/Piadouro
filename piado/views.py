04b0467fbd343c75b9063f644704aab78972b061
Eduardo Reinheimer <du428@hotmail.com>
2020-09-01 22:25:36 -0300

    Adicionada possibilidade de deletar piado

--- piado/views.py
+++ piado/views.py
@@ -1,7 +1,8 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from piado.models import Piado
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseNotAllowed, HttpResponseRedirect
 
 
 class PiadoCreate(LoginRequiredMixin, CreateView):
@@ -14,3 +15,18 @@ class PiadoCreate(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse('perfil', args=[self.request.user.username])
+
+class PiadoDelete(LoginRequiredMixin, DeleteView):
+    model = Piado
+    success_url = reverse_lazy('home')
+    
+    def get(self, request, *args, **kwargs):
+        return self.post(self, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.usuario.id != request.request.user.id:
+            raise HttpResponseNotAllowed()
+        success_url = self.get_success_url()
+        self.object.delete()
+        return HttpResponseRedirect(success_url)

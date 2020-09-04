247b4a98f4ab72ce1b5405b78345cf89f747eeaf
Eduardo Reinheimer <du428@hotmail.com>
2020-09-03 22:17:20 -0300

    Adiciona like e repiado

--- piado/views.py
+++ piado/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from piado.models import Piado
 from django.urls import reverse, reverse_lazy
@@ -30,4 +30,32 @@ class PiadoDelete(LoginRequiredMixin, DeleteView):
             raise PermissionDenied()
         success_url = self.get_success_url()
         self.object.delete()
+        return HttpResponseRedirect(success_url)
+
+class PiadoLike(LoginRequiredMixin, UpdateView):
+    model = Piado
+
+    def get(self, request, *args, **kwargs):
+        success_url = reverse('perfil', args=[request.GET['next_user']])
+        self.object = self.get_object()
+        self
+        if self.object.curtidas.filter(id = request.user.perfil.id).exists():
+            self.object.curtidas.remove(request.user.perfil.id)
+        else: 
+            self.object.curtidas.add(request.user.perfil.id)
+        
+        return HttpResponseRedirect(success_url)
+
+class RePiado(LoginRequiredMixin, UpdateView):
+    model = Piado
+
+    def get(self, request, *args, **kwargs):
+        success_url = reverse('perfil', args=[request.GET['next_user']])
+        self.object = self.get_object()
+        self
+        if self.object.repiados.filter(id = request.user.perfil.id).exists():
+            self.object.repiados.remove(request.user.perfil.id)
+        else: 
+            self.object.repiados.add(request.user.perfil.id)
+        
         return HttpResponseRedirect(success_url)

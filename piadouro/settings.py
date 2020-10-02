e7a6012ca817e4179063fcd37737d13c7741b85b
Eduardo Reinheimer <du428@hotmail.com>
2020-10-01 22:02:37 -0300

    Adicionando arquivos necessarios para deploy no heroku

--- piadouro/settings.py
+++ piadouro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'piadouro', 'imagens')
 LOGIN_URL = '/admin/login'
 LOGOUT_REDIRECT_URL = LOGIN_URL
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
